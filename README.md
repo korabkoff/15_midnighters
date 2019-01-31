5245eb9a905bf4e699bba653f6a2dbe80d744179
korabkoff <korabkoff@gmail.com>
2019-01-31 20:12:11 +0300

    Fill out given functions and readme

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+This script shows names of those who send tasks for check at Devman.org
+at night from 0:00 to 5:00.
+
+# How to run
+script require python3.5
+Example of script launch on Linux, Python 3.5:
+```#!bash
+python seek_dev_nighters.py
+demantick
+8d09b188684c43d5
+isaikin_photo
+9eda6aca4de44b13
+a.v.haurylenka
+...
+```
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,32 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+def load_attempts(url):
+    first_page = str(1)
+    pages = requests.get(url+first_page).json()['number_of_pages']
+    for page in range(pages):
+        yield requests.get('{}{}'.format(url, str(page+1))).json()['records']
+
+
+def get_midnighters(records_generator):
+    owls = []
+    for record in records_generator:
+        for idx in range(len(record)):
+            utc_timestamp = record[idx]['timestamp']
+            the_timezone = pytz.timezone(record[idx]['timezone'])
+            username = record[idx]['username']
+            utc_date_time = pytz.utc.localize(datetime.utcfromtimestamp(utc_timestamp))
+            local_date_time = utc_date_time.astimezone(the_timezone)
+            hours = str(local_date_time)[11:13]
+            last_night_hour = 6
+            if int(hours) < last_night_hour and username not in owls:
+                owls.append(username)
+    return owls
 
 if __name__ == '__main__':
-  pass
+    url = 'http://devman.org/api/challenges/solution_attempts/?page='
+
+    for name in get_midnighters(load_attempts(url)):
+        print(name)
